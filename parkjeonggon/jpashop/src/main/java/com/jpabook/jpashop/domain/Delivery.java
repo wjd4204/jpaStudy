6c7cc8dc06dbdd5d9f099b27aae37d9b5e4480f4
wjdrhs3473 <110841041+wjd4204@users.noreply.github.com>
2024-04-06 01:05:25 +0900

    Section9. 값 타입 (#3)

--- parkjeonggon/jpashop/src/main/java/com/jpabook/jpashop/domain/Delivery.java
+++ parkjeonggon/jpashop/src/main/java/com/jpabook/jpashop/domain/Delivery.java
@@ -8,10 +8,43 @@ public class Delivery extends BaseEntity {
     @Id
     @GeneratedValue
     private Long id;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
+    private DeliveryStatus deliveryStatus;
 
     @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public DeliveryStatus getDeliveryStatus() {
+        return deliveryStatus;
+    }
+
+    public void setDeliveryStatus(DeliveryStatus deliveryStatus) {
+        this.deliveryStatus = deliveryStatus;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
